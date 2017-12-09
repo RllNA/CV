9d067c01fab6f88cb53e2f597901c103283263d8
Riina <riina.kokko@gmail.com>
2017-12-09 23:59:40 +0200

    Commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+//Initializing Firebase
+export const FirebaseConfig = {
+    apiKey: "AIzaSyCOsYfyCQU5w9RMajOixrF0HVabQCpJdXA",
+    authDomain: "cvapp-b17be.firebaseapp.com",
+    databaseURL: "https://cvapp-b17be.firebaseio.com",
+    projectId: "cvapp-b17be",
+    storageBucket: "",
+    messagingSenderId: "1043917793352"
+    };
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from "../../models/users";
+import * as firebase from 'firebase/app';
 
 @IonicPage()
 @Component({
@@ -15,11 +11,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  twitterLoggedIn = false;
+  googleLoggedIn = false;
+  facebookLoggedIn = false;
+
+  user = {} as User;
+
+  constructor(private alertCtrl: AlertController, private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  //Creating alert
+  alert(message: string){
+    this.alertCtrl.create({
+      title: 'Info',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
   }
 
+  //Function to log the user in
+  async loginUser(user: User){
+    try {
+    const result = this.fire.auth.signInWithEmailAndPassword(user.email, user.password);
+    if (result) {
+      //this.alert('You are now logged in!')
+      //Navigating to inside of the app
+      this.navCtrl.setRoot('MenuPage');
+    }
+  } catch (e) {
+    this.alert(e.message);
+    console.error(e);
+  }
+}
+
+  //Function to move to the register page
+  doRegister(){
+    this.navCtrl.push('RegisterPage');
+  }
+
+  //Facebook login
+  loginFacebook(){
+    this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    .then( res => {
+      this.facebookLoggedIn = true;
+      this.navCtrl.setRoot('MenuPage');
+    })
+  }
+  
+  //Google login
+  loginGoogle(){
+    this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then( res => {
+      this.googleLoggedIn = true;
+      this.navCtrl.setRoot('MenuPage');
+    })
+  }
+  
+  //Twitter login
+  loginTwitter(){
+    this.fire.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
+    .then( res => {
+      this.twitterLoggedIn = true;
+      this.navCtrl.setRoot('MenuPage');
+    })
+  }
 }
